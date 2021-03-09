778b5ebc68f2e0a8c438f8547f1f04431fefacf8
Kurikania <65830166+Kurikania@users.noreply.github.com>
2021-03-09 17:00:08 +0300

    small fix

--- api/db.js
+++ api/db.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,  useUnifiedTopology: true})
+
+mongoose.connect( process.env.DATABASE_URL|| 'mongodb://localhost/test', {useNewUrlParser: true,  useUnifiedTopology: true})
+.then(()=>{
+    console.log("Connected to the Database. Yayzow!");
+})
+.catch(err => {
+    console.log(err);
+});
+
+//mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,  useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', error => console.log(error))
 db.once('open', () => console.log("Connected to mongoose"))
